ca8e0309123571e9274531a2638262962f4a0df5
CTFUKI <callumjo41@gmail.com>
2026-03-09 20:10:57 +0000

    Changed model_trainer.py to use Pyod
    Using python package over training my own model. Still using auto encoder just not building my own.

--- model/model_trainer.py
+++ model/model_trainer.py
@@ -1,36 +1,30 @@
-import numpy as np 
-import tensorflow as tf
-from tensorflow.keras import layers, losses
+import csv
+import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from pyod.models.auto_encoder import AutoEncoder
+
+#Import the training data
+df = pd.read_csv('normal_betting_behaviour.csv')
+
+data = df.select_dtypes(include=['number']).values
+
+print(data)
 
 
-#Prepare the data
+if __name__ == "__main__":
+    contamination = 0.1
 
+    x_train, x_test, = train_test_split(data, test_size=0.2, random_state=42)
 
+    clf_name = 'AutoEncoder'
+    clf_name = AutoEncoder(epoch_num=30, contamination=contamination)
+    clf.fit(x_train)
 
-#Define the autoencoder architetcure
-class AnomalyDetector(tf.keras.Model):
-    def __init__(self, input_dim):
-        super(AnomalyDetector, self).__init__()
+    y_train_pred = clf.labels_
+    y_train_scores = clf.decision_scores
 
+    y_test_pred = clf.predict(x_test)
+    y_test_scores = clf.decision_function(x_test)
 
-        # Encoder squsahses data down into bottle neck (model gets more strict around what it considers core information) and latent space
-        self.encoder = tf.keras.Sequential([
-            layers.Dense(32, activation='relu'),
-            layers.Dense(16, activation='relu'),
-            layers.Dense(8, activation='relu'),
-        ])
-
-
-        # Decoder reconstructs the orginal data from the bottle neck -> if the reconstrciton looks strange then we detect anomalie
-        self.decoder = tf.keras.Sequential([
-            layers.Dense(16, activation='relu'),
-            layers.Dense(32, activation='relu'),
-            layers.Dense(input_dim, activation='sigmoid')])
-
-    def call(self, x):
-        encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
-        return decoded
-
+    print(f"Training: {sum(y_train_pred)} anomalies out of {len(y_train_pred)}")
+    print(f"Test (normal): {sum(y_test_pred)} anomalies out of {len(y_test_pred)}")
